aca9f0f45faf252475eff05cca7a576be8ce97d0
jvanlier <whizzrd@users.noreply.github.com>
2007-07-29 23:01:18 +0000

    Imported phakt-2_0_66

---
+++ doc/tutorials/mysql/README.txt
@@ -0,0 +1,3 @@
+This is the PHAkt2 MX tutorial.
+As we have improved the code lately in PHAkt, the tutorial files might be broken.
+Everything will be corrected until 20th July 2002.
---
+++ www/demo/mysql/search.php
@@ -0,0 +1,33 @@
+<?php //PHP ADODB document - made with PHAkt 2.0.37?>
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<form name="form1" method="post" action="list.php">
+  <p>&nbsp; </p>
+  <table width="32%" border="0">
+    <tr>
+      <td width="12%">Name:</td>
+      <td width="88%"><input name="name" type="text" id="name"></td>
+    </tr>
+    <tr>
+      <td>Code:</td>
+      <td><input name="code" type="text" id="code"></td>
+    </tr>
+    <tr>
+      <td>Address:</td>
+      <td><input name="address" type="text" id="address"></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><input type="submit" name="Submit" value="Submit">
+        <input type="reset" name="Submit2" value="Reset"></td>
+    </tr>
+  </table>
+  <p>&nbsp; </p>
+  </form>
+</body>
+</html>
