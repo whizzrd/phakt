2d499280efb13bb6a373b4eb16d4ae6f09e9627b
jvanlier <jvanlier@ba1a3169-0a36-0410-8252-a586ab222f6b>
2007-08-04 11:14:44 +0000

    Branched phakt-2_2_0

---
+++ branches/phakt/2.2.0/www/demo/pgsql/admin/insert.php
@@ -0,0 +1,78 @@
+<?php require_once('../Connections/test.php'); ?>
+<?php require_once('../includes/functions.inc.php'); ?>
+<?php
+// *** Restrict Access To Page: Grant or deny access to this page
+$KT_authorizedUsers=" 10";
+$KT_authFailedURL="login.php";
+$KT_grantAccess=0;
+session_start();
+if (isset($HTTP_SESSION_VARS["KT_Username"])) {
+  if (false || !(isset($HTTP_SESSION_VARS["KT_userAuth"])) || $HTTP_SESSION_VARS["KT_userAuth"]=="" || strpos($KT_authorizedUsers, $HTTP_SESSION_VARS["KT_userAuth"])) {
+    $KT_grantAccess = 1;
+  }
+}
+if (!$KT_grantAccess) {
+  $KT_qsChar = "?";
+  if (strpos($KT_authFailedURL, "?")) $KT_qsChar = "&";
+  $KT_referrer = $HTTP_SERVER_VARS["REQUEST_URI"];
+  $KT_authFailedURL = $KT_authFailedURL . $KT_qsChar . "accessdenied=" . urlencode($KT_referrer);
+  KT_redir($KT_authFailedURL);
+}
+
+// build the form action
+$editFormAction = $HTTP_SERVER_VARS['PHP_SELF'] . (isset($HTTP_SERVER_VARS['QUERY_STRING']) ? "?" . $HTTP_SERVER_VARS['QUERY_STRING'] : "");
+
+if ((isset($HTTP_POST_VARS["MM_insert"])) && ($HTTP_POST_VARS["MM_insert"] == "form1")) {
+  $insertSQL = sprintf("INSERT INTO users_usr (username_usr, password_usr, level_usr) VALUES (%s, %s, %s)",
+                       GetSQLValueString($HTTP_POST_VARS['username_usr'], "text"),
+                       GetSQLValueString($HTTP_POST_VARS['password_usr'], "text"),
+                       GetSQLValueString($HTTP_POST_VARS['level_usr'], "int"));
+
+  $Result1 = $test->Execute($insertSQL) or die($test->ErrorMsg());
+
+  $insertGoTo = "index.php";
+  if (isset($HTTP_SERVER_VARS['QUERY_STRING'])) {
+    $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
+    $insertGoTo .= $HTTP_SERVER_VARS['QUERY_STRING'];
+  }
+  KT_redir($insertGoTo);
+}
+ //PHP ADODB document - made with PHAkt 2.0.60?>
+
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<form method="post" name="form1" action="<?php echo $editFormAction; ?>">
+  <table align="center">
+    <tr valign="baseline"> 
+      <td nowrap align="right">Id_usr:</td>
+      <td></td>
+    </tr>
+    <tr valign="baseline"> 
+      <td nowrap align="right">Username_usr:</td>
+      <td><input type="text" name="username_usr" value="" size="32"></td>
+    </tr>
+    <tr valign="baseline"> 
+      <td nowrap align="right">Password_usr:</td>
+      <td><input type="text" name="password_usr" value="" size="32"></td>
+    </tr>
+    <tr valign="baseline"> 
+      <td nowrap align="right">Level_usr:</td>
+      <td><input type="text" name="level_usr" value="" size="32"></td>
+    </tr>
+    <tr valign="baseline"> 
+      <td nowrap align="right">&nbsp;</td>
+      <td><input type="submit" value="Insert Record"></td>
+    </tr>
+  </table>
+  <input type="hidden" name="MM_insert" value="form1">
+</form>
+<p><a href="index.php">Back</a></p>
+  
+
+</body>
+</html>
